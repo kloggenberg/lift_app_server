57898a6057736bfa23d3520c7e67fb0e5fb9d5c3
kloggenberg023 <kloggenberg023@student.wethinkcode.co.za>
2024-11-05 19:03:54 +0200

    Adding TestClient

--- src/main/java/code/client/TestClient.java
+++ src/main/java/code/client/TestClient.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
-public class Client {
+public class TestClient {
     public static void main(String[] args) {
         String serverAddress = "localhost";
         int port = 5000;
