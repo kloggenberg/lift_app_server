0a2573363f5396d7526887424af08763a7c7df87
kloggenberg023 <kloggenberg023@student.wethinkcode.co.za>
2024-11-05 20:08:05 +0200

    Updating AuthenticationService

--- src/main/java/code/authentication/AuthenticationService.java
+++ src/main/java/code/authentication/AuthenticationService.java
@@ -4,17 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AuthenticationService {
-    // Example credentials (In a real application, use a secure storage or database)
     private Map<String, String> userDatabase;
 
     public AuthenticationService() {
         userDatabase = new HashMap<>();
-        // Adding some example users
         userDatabase.put("user1", "password1");
         userDatabase.put("user2", "password2");
     }
-
-    // Method to check username and password
     public boolean authenticate(String username, String password) {
         return userDatabase.containsKey(username) && userDatabase.get(username).equals(password);
     }
